e9c113041a8f3492cae392e33f61693add64d3fc
Raj Shekhar <rajshekhar2may2002@gmail.com>
2024-12-24 12:28:30 +0530

    initial student repository setup with capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  // server: {
+  //   url: "http://192.168.56.1:5173/",
+  //   cleartext: true,
+  // },
+
+  appId: "io.vacademy.student.app",
+  appName: "ssdc_vacademy_student",
+  webDir: "dist",
+};
+
+export default config;
