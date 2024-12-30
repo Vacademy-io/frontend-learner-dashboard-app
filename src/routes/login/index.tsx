7099b21c585390cb11576e4baab2ecb4edd160b2
Raj Shekhar <rajshekhar2may2002@gmail.com>
2024-12-30 16:35:43 +0530

    on refresh direct reroute to dashboard

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -2,19 +2,19 @@ import { createFileRoute } from "@tanstack/react-router";
 import { LoginForm } from "@/components/common/LoginPages/sections/login-form";
 // import { SplashScreen } from "@/components/common/LoginPages/layout/splash-container";
 
-// import { getTokenFromCookie } from "@/lib/auth/sessionUtility";
-// import { TokenKey } from "@/constants/auth/tokens";
-// import { isNullOrEmptyOrUndefined } from "@/lib/utils";
+import { getTokenFromCookie } from "@/lib/auth/sessionUtility";
+import { TokenKey } from "@/constants/auth/tokens";
+import { isNullOrEmptyOrUndefined } from "@/lib/utils";
 
 export const Route = createFileRoute("/login/")({
   loader: () => {
-    // const accessToken = getTokenFromCookie(TokenKey.accessToken);
-    // if (!isNullOrEmptyOrUndefined(accessToken)) {
-    //     throw redirect({ to: "/dashboard" });
-    // }
+    const accessToken = getTokenFromCookie(TokenKey.accessToken);
+    if (!isNullOrEmptyOrUndefined(accessToken)) {
+        throw new Error("Redirect to /dashboard");
+    }
     return;
   },
-  component: RouteComponent,
+  component: RouteComponent
 });
 
 function RouteComponent() {
