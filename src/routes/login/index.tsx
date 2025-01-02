29d80482d113c0ffe767a8b2195f72f9d3407744
Raj Shekhar <rajshekhar2may2002@gmail.com>
2025-01-02 12:30:14 +0530

    if access token is present redirecting to the dashboard

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,43 +1,14 @@
-// import { createFileRoute } from "@tanstack/react-router";
-// import { LoginForm } from "@/components/common/LoginPages/sections/login-form";
-// // import { SplashScreen } from "@/components/common/LoginPages/layout/splash-container";
-
-// import { getTokenFromCookie } from "@/lib/auth/sessionUtility";
-// import { TokenKey } from "@/constants/auth/tokens";
-// import { isNullOrEmptyOrUndefined } from "@/lib/utils";
-
-// export const Route = createFileRoute("/login/")({
-//   loader: () => {
-//     const accessToken = getTokenFromCookie(TokenKey.accessToken);
-//     if (!isNullOrEmptyOrUndefined(accessToken)) {
-//         throw new Error("Redirect to /dashboard");
-//     }
-//     return;
-//   },
-//   component: RouteComponent
-// });
-
-// function RouteComponent() {
-//   return (
-//       <LoginForm />
-//   );
-// }
-
-
-
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { LoginForm } from "@/components/common/LoginPages/sections/login-form";
-// import { SplashScreen } from "@/components/common/LoginPages/layout/splash-container";
-
 import { Storage } from "@capacitor/storage";
 import { TokenKey } from "@/constants/auth/tokens";
 import { isNullOrEmptyOrUndefined } from "@/lib/utils";
 
 export const Route = createFileRoute("/login/")({
   loader: async () => {
-    const { value: accessToken } = await Storage.get({ key: TokenKey.accessToken });
+        const { value: accessToken } = await Storage.get({ key: TokenKey.accessToken });
     if (!isNullOrEmptyOrUndefined(accessToken)) {
-      throw new Error("Redirect to /dashboard");
+      throw redirect({ to: "/dashboard" });
     }
     return;
   },
