e95ca2ff47dfaa6c742327f12c7791f880dbf9f9
Raj Shekhar <rajshekhar2may2002@gmail.com>
2025-01-02 15:00:20 +0530

    updated code

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,9 +1,5 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute} from "@tanstack/react-router";
 import { LoginForm } from "@/components/common/LoginPages/sections/login-form";
-import { Storage } from "@capacitor/storage";
-import { TokenKey } from "@/constants/auth/tokens";
-import { isNullOrEmptyOrUndefined } from "@/lib/utils";
-
 export const Route = createFileRoute("/login/")({
   
   component: RouteComponent,
