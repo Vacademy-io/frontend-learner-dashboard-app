ecac497398e1435d04c58f7dc17b3a00e3379014
Shubhamydv-07 <yshubh140704@gmail.com>
2026-01-14 20:06:52 +0530

    fix:instituteId dynamic

--- src/services/study-library/getStudyLibraryDetails.ts
+++ src/services/study-library/getStudyLibraryDetails.ts
@@ -1,8 +1,7 @@
 // services/study-library/getStudyLibraryDetails.ts
 import authenticatedAxiosInstance from "@/lib/auth/axiosInstance";
-import { INIT_STUDY_LIBRARY, INSTITUTE_ID } from "@/constants/urls";
+import { INIT_STUDY_LIBRARY } from "@/constants/urls";
 import { useStudyLibraryStore } from "@/stores/study-library/use-study-library-store";
-import { getTokenFromStorage } from "@/lib/auth/sessionUtility";
 import { getInstituteId } from "@/utils/study-library/get-list-from-stores/getPackageSessionId";
 
 export const fetchStudyLibraryDetails = async (packageSessionId: string) => {
@@ -10,7 +9,7 @@ export const fetchStudyLibraryDetails = async (packageSessionId: string) => {
     const idFromDetails = await getInstituteId();
 
     // Fallback to stand-alone key or hardcoded ID if details are missing
-    const effectiveInstituteId = idFromDetails || INSTITUTE_ID;
+    const effectiveInstituteId = idFromDetails;
 
 
     const response = await authenticatedAxiosInstance.get(INIT_STUDY_LIBRARY, {
